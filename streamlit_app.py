11a37d5cb99f01224d91908e638e904c3ff74737
georgeesp-data <114700691+georgeesp-data@users.noreply.github.com>
2022-10-01 00:13:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
-streamlit.title('My Mom''s New Healthy Diner')
+streamlit.title('My Parent\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
