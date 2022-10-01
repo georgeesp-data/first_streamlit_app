ef5fe297608b796f4f8206f50bb38bb7ed579965
georgeesp-data <114700691+georgeesp-data@users.noreply.github.com>
2022-10-01 00:02:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('U+1F957 Omega 3 & Blueberry Oatmeal')
+streamlit.text(':U+1F957: Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
