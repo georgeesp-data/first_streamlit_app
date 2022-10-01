a6e10c3eeed820421b20ee8c350d4cf715076be7
georgeesp-data <114700691+georgeesp-data@users.noreply.github.com>
2022-10-01 00:10:23 -0700

    Update streamlit_app.py
    Added emojis to menu items
    Added Avocado toast menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,12 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom''s New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.dataframe(my_fruit_list)
